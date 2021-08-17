755dc28a5bd14f16b42234cf394bca2da83adafc
cuongtien00 <cuonghiep000@gmail.com>
2021-08-17 11:48:03 +0700

    4

---
+++ js.js
@@ -0,0 +1,9 @@
+function laydulieu() {
+    // a = document.getElementById("demo2").innerText ="Hoang tien cuong";
+    // a = document.getElementById("demo2").innerHTML ="<h1>Hoang tien cuong</h1>";
+
+    a=prompt("";)
+    alert(a);
+
+    
+}
