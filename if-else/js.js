034e1ef22fe2b53bf3bdc3b4a93f9cfdce8427ac
cuongtien00 <cuonghiep000@gmail.com>
2021-08-23 15:47:48 +0700

    add maytinhdongian

--- if-else/js.js
+++ if-else/js.js
@@ -6,29 +6,9 @@ function init() {
 }
 function sohang(e) {
     let sohang = e.getAttribute("value");
- // let result=0;
-// switch (sohang) {
-//     case "1":
-//     case "2":
-//     case "3":
-//     case "4":
-//     case "5":
-//     case "6":
-//     case "7":
-//     case "8":
-//     case "9":
-//     case "0":
-//         result = sohang;
-//     break;
-//
-// }
     document.getElementById("nhap").value += sohang;
 }
 function tinhtoan() {
     nhap = document.getElementById("nhap").value;
     document.getElementById("nhap").value = eval(nhap);
-}
-function clear() {
-  xoa = document.getElementById("nhap").innerHTML;
-   
 }
